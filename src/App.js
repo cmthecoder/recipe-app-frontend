be5ee2bd0bc4c8dc0402f35a27daf4acb5550489
Chris Mems <christopher.mems@.com>
2023-04-26 13:34:18 -0500

    create components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Auth from './pages/Auth';
+import CreateRecipe from './pages/CreateRecipe';
+import SavedRecipes from './pages/SavedRecipes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/create-recipe' element={<CreateRecipe />} />
+        <Route path='/saved-recipes' element={<SavedRecipes />} />
+      </Routes>
     </div>
   );
 }
